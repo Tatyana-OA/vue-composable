ff4ab663dce8873ced814823f73217c06c3bbb7a
TAsenova <tasenova@mentormate.com>
2024-10-09 13:59:40 +0300

    useRefHistory

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,37 @@
 <script setup>
 import { useMouse } from "./composables/mouse";
+import { useTitle, useRefHistory } from "@vueuse/core";
+import { ref } from "vue";
+
+// Custom Composable
 const { x, y } = useMouse();
+// Page Title
+const title = useTitle("Mouse Position", { titleTemplate: "%s | My Site" });
+// Ref History
+const count = ref(0);
+const { undo } = useRefHistory(count);
+
+const incrementCount = () => {
+  count.value++;
+};
+
+const undoCount = () => {
+  undo();
+};
+
+const clearCount = () => {
+  count.value = 0;
+};
 </script>
 
 <template>
-  <main>Mouse Position: X: {{ x }}; Y: {{ y }}</main>
+  <main>
+    <h1>Mouse Position: X: {{ x }}; Y: {{ y }}</h1>
+    <!-- Change Page title dynamically -->
+    <input type="text" v-model="title" />
+    <h3>This is count: {{ count }}</h3>
+    <button @click="incrementCount">Increment Count</button>
+    <button @click="undoCount">Undo Change</button>
+    <button @click="clearCount">Clear Count</button>
+  </main>
 </template>
